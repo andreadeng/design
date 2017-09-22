514471730399da4b40246d3cdf8cf683a738589a
Andrea Deng <andrea.deng@berkeley.edu>
2017-09-22 12:45:01 -0700

    Use latest  material ui

--- src/App.js
+++ src/App.js
@@ -4,34 +4,23 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      txt: 'this is the state txt'
+      items: []
     }
   }
-  update( e ){
-    this.setState({txt: e.target.value})
-  }
+  
+componentWillMount() {
+  fetch( '')
+}
+
   render(){
+    let items = this.state.items
     return (
       <div>
-        <Widget update={this.update.bind(this)} />
-        <Button > I <Heart /> React</Button>
-        <h1>{this.state.txt}</h1>
+        
       </div>
     )
   }
 }
 
-const Widget = (props) =>
-  <input type="text" onChange={props.update} />
-
-const Button = (props) => <button>{props.children}</button>
-
-const Title = (props) => <h1> Title: {props.text}</h1>
-
-class Heart extends React.Component {
-  render(){
-    return <span> &hearts;</span>
-  }
-}
 
 export default App
