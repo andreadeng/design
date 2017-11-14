b11e268e472acb9d5ea923192088ee38064eda26
Andrea Deng <andrea.deng@berkeley.edu>
2017-11-14 12:43:55 -0800

    Switched browserHistory to hashHistory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {IndexRoute, Router, Route, browserHistory} from 'react-router';
+import {IndexRoute, Router, Route, hashHistory} from 'react-router';
 
 import './style.css';
 
@@ -11,7 +11,7 @@ import Challenges from './containers/Challenges';
 class List extends React.Component {
   render() {
     return (
-      <Router history={browserHistory}>
+      <Router history={hashHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={Home} />
           <Route path="challenges" component={Challenges} />
